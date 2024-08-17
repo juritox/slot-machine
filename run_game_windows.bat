08c7c383727006ee933a32f9957415883e9fc9b5
Tomáš Juříček <165699241+juritox@users.noreply.github.com>
2024-08-17 20:43:21 +0200

    Update run_game_windows.bat
    - if main.pyw already exists then script now checks if main.py has not been updated and if so then it creates also updated version of main.pyw

--- run_game_windows.bat
+++ run_game_windows.bat
@@ -1,5 +1,22 @@
 @echo off
+setlocal enabledelayedexpansion
+
+REM Check if the main.pyw file exists
 if not exist "src\main.pyw" (
+    REM If main.pyw does not exist, copy main.py to main.pyw
     copy "src\main.py" "src\main.pyw"
+) else (
+    REM Get the last modified timestamps of main.py and main.pyw
+    for %%I in (src\main.py) do set mainpydate=%%~tI
+    for %%I in (src\main.pyw) do set mainpywdate=%%~tI
+
+    REM Compare the timestamps and update main.pyw if main.py is newer
+    if "!mainpydate!" GTR "!mainpywdate!" (
+        copy /Y "src\main.py" "src\main.pyw"
+    )
 )
+
+REM Run the game using pythonw without opening a command prompt window
 start "" pythonw src\main.pyw
+
+endlocal
